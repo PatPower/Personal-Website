0b8c6189e94595d51b18d173997d2a3f8893795a
PatPower <patent.li@mail.utoronto.ca>
2022-02-26 13:46:49 -0500

    first commit

---
+++ src/containers/skills/Skills.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './skills.css'
+
+const Skills = () => {
+    return (
+        <div className='skills-container' id='skills'>
+            <h1>
+                Skills
+            </h1>
+            <div className="skills-container-categories">
+                <div className="skills-category">
+                    <h3>
+                        Languages
+                    </h3>
+                </div>
+                <div className="skills-category">
+                <h3>
+                        Tools
+                    </h3>
+                </div>
+                <div className="skills-category">
+                <h3>
+                        
+                    </h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
